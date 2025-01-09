4fb837fdab20113abd5ede053944d3293d6ed05e
LidianoWagner <lidiano.xavier@hotmail.com>
2025-01-09 08:59:55 -0300

    Listando transacoes e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,44 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    // Verificando se os dados estão sendo carregados corretamente
+    console.log("Carregando os dados...");
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 3000,
+          createdAt: new Date("2022-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 2000,
+          createdAt: new Date("2022-02-28 11:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api"; // Definindo o namespace para '/api'
 
     // Definindo a rota GET para '/api/transactions'
     this.get("/transactions", () => {
-      return this.schema.all("transacion");
+      // Verificando se o servidor MirageJS tem as transações
+      console.log("Transações carregadas:", this.schema.all("transaction"));
+      return this.schema.all("transaction");
     });
 
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
-
       return schema.create("transaction", data);
     });
   },
